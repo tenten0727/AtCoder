66e310b2eea92989b72ec84e3f511c1c791505b3
吉川天斗 <tenten0727@icloud.com>
2021-03-19 22:34:14 +0900

    ABC190 D AC

--- AtCoder Beginner Contest 190/D.py
+++ AtCoder Beginner Contest 190/D.py
@@ -1,14 +1,27 @@
 N = int(input())
 
-#約数を求める
-def make_divisors(n):
-    lower_divisors , upper_divisors = [], []
-    i = 1
-    while i*i <= n:
-        if n % i == 0:
-            lower_divisors.append(i)
-            if i != n // i:
-                upper_divisors.append(n//i)
-        i += 1
-    return lower_divisors + upper_divisors[::-1]
+while N % 2 == 0:
+    N //= 2
 
+ans = 0
+for i in range(1, int(N**0.5)+1):
+    if N % i == 0:
+        ans += 2
+
+if int(N**0.5)*int(N**0.5) == N:
+    ans -= 1
+
+print(ans*2)
+
+
+#約数を求める
+# def make_divisors(n):
+#     lower_divisors , upper_divisors = [], []
+#     i = 1
+#     while i*i <= n:
+#         if n % i == 0:
+#             lower_divisors.append(i)
+#             if i != n // i:
+#                 upper_divisors.append(n//i)
+#         i += 1
+#     return lower_divisors + upper_divisors[::-1]
