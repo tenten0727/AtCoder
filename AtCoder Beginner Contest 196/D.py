e4943aba43330ed1f7400370461cdc61d9f579f4
吉川天斗 <tenten0727@icloud.com>
2021-04-12 19:57:03 +0900

    ABC198 D

--- AtCoder Beginner Contest 196/D.py
+++ AtCoder Beginner Contest 196/D.py
@@ -1,19 +1,30 @@
 H, W, A, B = map(int, input().split())
-ans = 0
-def dfs(i, bit, A, B):
-    if i == H * W:
-        global ans
-        ans += 1
-        return
-    if bit >> i & 1:
-        dfs(i + 1, bit, A, B)
-        return
-    if B:
-        dfs(i + 1, bit | 1 << i, A, B - 1)
-    if A:
-        if i % W != W - 1 and not bit & 1 << (i + 1):
-            dfs(i + 1, bit | 1 << i | 1 << (i + 1), A - 1, B)
-        if i + W < H * W:
-            dfs(i + 1, bit | 1 << i | 1 << (i + W), A - 1, B)
-dfs(0, 0, A, B)
-print(ans)
+used = list(list(False for _ in range(W)) for _ in range(H))
+
+def dfs(i, j, a, b):
+	if a < 0 or b < 0:
+		return 0
+	if j == W:
+		j = 0
+		i += 1
+	if i == H:
+		return 1
+	if used[i][j] == True:
+		return dfs(i, j+1, a, b)
+
+	ans = 0
+	used[i][j] = True
+	ans += dfs(i, j+1, a, b-1)
+	if j+1 < W and not used[i][j+1]:
+		used[i][j+1] = True
+		ans += dfs(i, j+1, a-1, b)
+		used[i][j+1] = False
+	if i+1 < H and not used[i+1][j]:
+		used[i+1][j] = True
+		ans += dfs(i, j+1, a-1, b)
+		used[i+1][j] = False
+	used[i][j] = False
+	return ans
+
+
+print(dfs(0, 0, A, B))
