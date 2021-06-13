111862a20e8560358548b347837240bf5bf1f61e
tenten0727 <tenten0727@icloud.com>
2021-06-13 21:00:23 +0900

    ABC 202 ABCD

--- AtCoder Beginner Contest 202/C.py
+++ AtCoder Beginner Contest 202/C.py
@@ -1,17 +1,12 @@
-import collections
-
 N = int(input())
+
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 C = list(map(int, input().split()))
 
-
-D = [B[i-1] for i in C]
-Dcount = collections.Counter(D)
-
 ans = 0
-
-for a in A:
-    ans += Dcount[a]
-
+for j, c in enumerate(C):
+    b = B[c-1]
+    ans += A.count(b)
+    
 print(ans)
--- AtCoder Beginner Contest 202/D.py
+++ AtCoder Beginner Contest 202/D.py
@@ -1,33 +1,39 @@
 import math
 
-def combinations_count(n, r):
-    return math.factorial(n) // (math.factorial(n - r) * math.factorial(r))
-
 A, B, K = map(int, input().split())
 
-ans = ''
-N = A + B
-count = 0
-a = A
-b = B
-
-for n in range(N, 0, -1):
-    if a <= 0:
-        for _ in range(b):
-            ans = ans + 'b'
-        break
-    
-    if b <= 0:
-        for _ in range(a):
-            ans = ans + 'a'
-        break
-
-    if count + combinations_count(a+b-1, b) >= K:
-        ans = ans + 'a'
-        a -= 1
-    else:
-        count += combinations_count(a+b-1, b)
-        ans = ans + 'b'
-        b -= 1
-    
-print(ans)
+def func():
+    count = 0
+    i = 0
+    a = A
+    b = B
+    s = ''
+    while True:
+        i += 1
+        
+        if i == A:
+            s = s + 'b'
+            if i != 1:
+                count += math.factorial(b+i-1) / math.factorial(i-1) / math.factorial(b) + 1
+            b = b-1
+            i = 0
+        if count + (math.factorial(b+i-1) / math.factorial(i-1) / math.factorial(b) + 1) > K:
+            sa = list('a' for _ in range(a - i))
+            s = s + ''.join(sa) + 'b'
+            if i != 1:
+                count += math.factorial(b+i-2) / math.factorial(i-2) / math.factorial(b) + 1
+            a = i
+            b = b-1
+            i = 0
+            
+        print(a, b, i, count)
+        if b == 0:
+            sa = list('a' for _ in range(a))
+            print(s + ''.join(sa))
+            break
+        if a == 0:
+            sb = list('b' for _ in range(b))
+            print(s + ''.join(sb))
+            break
+        
+func()
