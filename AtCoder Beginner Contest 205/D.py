3dd1b28692948da871f0eaaea1da7c46bb98c045
tenten0727 <tenten0727@icloud.com>
2021-06-13 23:54:30 +0900

    TLE

--- AtCoder Beginner Contest 205/D.py
+++ AtCoder Beginner Contest 205/D.py
@@ -1,12 +1,12 @@
-n, q = map(int, input().split())
-A = list(map(int, input().split()))
-K = list(int(input()) for _ in range(q))
+# n, q = map(int, input().split())
+# A = list(map(int, input().split()))
+# K = list(int(input()) for _ in range(q))
 
-def f(x, c):
-    count = sum([i <= x for i in A])
-    if count == c:
-        return x
-    return f(x+count-c, count)
+# def f(x, c):
+#     count = sum([i <= x for i in A])
+#     if count == c:
+#         return x
+#     return f(x+count-c, count)
 
-for k in K:
-    print(f(k, 0))
+# for k in K:
+#     print(f(k, 0))
