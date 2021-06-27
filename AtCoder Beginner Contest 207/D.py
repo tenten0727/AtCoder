dffbc0ac716b708531a80c5242e964c51eb112f1
TakatoYoshikawa <tenten0727@icloud.com>
2021-06-27 22:41:35 +0900

    ABC 207 D AC

--- AtCoder Beginner Contest 207/D.py
+++ AtCoder Beginner Contest 207/D.py
@@ -1,5 +1,5 @@
 import sys, re
-from math import ceil, floor, sqrt, pi, factorial, gcd
+from math import atan, ceil, cos, floor, sin, sqrt, pi, factorial, gcd, atan2
 from copy import deepcopy
 from collections import Counter, deque
 from heapq import heapify, heappop, heappush
@@ -25,23 +25,50 @@ MOD = 10 ** 9 + 7
 num_list = []
 str_list = []
 
+def mean(A):
+    return sum(A) / len(A)
+
 def main():
     n = i_input()
     AB = i_row_list(n)
+    A = [a for a, b in AB]
+    B = [b for a, b in AB]
     CD = i_row_list(n)
-    # CD.sort()
-
-    # for i in range(4):
-    #     for q in range(-28, 29):
-    #         for r in range(-28, 29):
-    #             AB = [[-b, a] for a, b in AB]
-    #             AB_plus = [[a + q, b + r] for a, b in AB]
-    #             AB_plus.sort()
-    #             if AB_plus == CD:
-    #                 print('Yes')
-    #                 exit()
-
-    # print('No')
+    C = [c for c, d in CD]
+    D = [d for c, d in CD]
+    
+    if n == 1:
+        print('Yes')
+    else:
+        A = [a - mean(A) for a in A]
+        B = [a - mean(B) for a in B]
+        CD = [[a - mean(C), b - mean(D)] for a, b in CD]
+        C = [a - mean(C) for a in C]
+        D = [a - mean(D) for a in D]
+        
+        for x, y in zip(A, B):
+            if x != 0 or y != 0:
+                break
+        # print(x, y)
+        for i in range(n):
+            xi, yi = C[i], D[i]
+            angle = atan2(yi, xi) - atan2(y, x)
+            
+            for j in range(n):
+                xj = A[j]*cos(angle)-B[j]*sin(angle)
+                yj = A[j]*sin(angle)+B[j]*cos(angle)
+                flag = False
+                for k in range(n):
+                    if abs(xj - CD[k][0]) <= 1e-5 and abs(yj - CD[k][1]) <= 1e-5:
+                        flag = True
+                        break
+                if not flag:
+                    break
+            if flag:
+                print('Yes')
+                exit()
+        
+        print('No')
 
 if __name__ == '__main__':
     main()
