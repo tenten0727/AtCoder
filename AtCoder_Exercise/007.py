de18950b32e500b4a9d3269f163aa06008c85976
tenten0727 <tenten0727@icloud.com>
2021-07-30 22:48:11 +0900

    Ex 007 AC

---
+++ AtCoder_Exercise/007.py
@@ -0,0 +1,48 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    n = i_input()
+    A = i_list()
+    q = i_input()
+    B = i_row(q)
+    A.sort()
+    
+    for b in B:
+        l = 0
+        r = n - 1
+        while r - l > 1:
+            mid = l + (r - l) // 2
+            if A[mid] > b:
+                r = mid
+            else:
+                l = mid
+        print(min(abs(A[l] - b), abs(A[r] - b)))
+        
+
+if __name__ == '__main__':
+    main()
