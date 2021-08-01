458f0e81f4e1cd18d4c9cd987a3a2a4fe692b186
TakatoYoshikawa <tenten0727@icloud.com>
2021-08-01 18:56:44 +0900

    Add shellscript for creating folder and file

---
+++ atcoder.sh
@@ -0,0 +1,27 @@
+#/usr/bin/bash
+
+start_atcoder () {
+    mkdir -p "$1"
+    cd "$1"
+    shift
+    for N in "$@";do
+        touch "$N.py"
+    done
+}
+
+file=()
+if [ "$3" = "" ]; then
+    file=("A" "B" "C" "D")
+else
+    for N in "${@:3}";do
+        file+=($N)
+    done
+fi
+
+if [ "$1" = "abc" ]; then
+    start_atcoder "AtCoder Beginner Contest $2" ${file[@]}
+elif [ "$1" = "arc"]
+    start_atcoder "AtCoder Regular Contest $2" ${file[@]}
+else
+    echo '第一引数が間違ってるー'
+fi
