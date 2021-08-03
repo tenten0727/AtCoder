b76c80d810de16bd7ae613a859d6a6f16c2ff68d
TakatoYoshikawa <tenten0727@icloud.com>
2021-08-03 21:52:07 +0900

    ABC 171 C virtualAC

---
+++ AtCoder Beginner Contest 171/C_virtual.py
@@ -0,0 +1,45 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+
+def main():
+    n = i_input() - 1
+    
+    ans = ''
+    i = 1
+    while True:
+        if n // (26**i) == 0:
+            break
+        n -= 26 ** i
+        i += 1
+    
+    ans = ''
+    for ii in range(i):
+        ans = chr(ord('a')+(n%26)) + ans
+        n //= 26
+
+    print(ans)
+
+if __name__ == '__main__':
+    main()
