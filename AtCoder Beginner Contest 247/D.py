770892a35cbb08a9935e83c839ac989df66469c6
吉川天斗 <tenten0727@icloud.com>
2022-05-04 19:34:04 +0900

    ABC247 D WA

---
+++ AtCoder Beginner Contest 247/D.py
@@ -0,0 +1,49 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+
+def main():
+    q = i_input()
+    Q = i_row_list(q)
+
+    d = deque([])
+    for query in Q:
+        if query[0] == 1:
+            _, x, c = query
+            d.append([c, x])
+        else:
+            _, c = query
+            ans = count = 0
+            while count < c:
+                _c, _x = d.popleft()
+                count += _c
+                ans += _x * _c
+            if count > c:
+                over = count - c
+                ans -= _x * over
+                d.appendleft([over, _x])
+            print(ans)
+
+if __name__ == '__main__':
+    main()
