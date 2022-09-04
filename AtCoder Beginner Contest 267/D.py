5e205658fea6167fd2a65d2ea5e0812bb12ab02c
tenten0727 <tenten0727@icloud.com>
2022-09-04 15:33:50 +0900

    ABC267 D virtualAC

--- AtCoder Beginner Contest 267/D.py
+++ AtCoder Beginner Contest 267/D.py
@@ -22,13 +22,25 @@ def lcm(a, b): return a * b // gcd(a, b)
 sys.setrecursionlimit(10 ** 6)
 INF = float('inf')
 MOD = 10 ** 9 + 7
-num_list = []
-str_list = []
 
 
 def main():
     # dp?
-    print(0)
+    n, m = i_map()
+    A = i_list()
+    
+    #dp[Aのリストの先頭からx個の中から][m個選んだ時の] = 最大値
+    dp = [[-INF] * (m+1) for _ in range(n+1)]
+    for i in range(n+1):
+        dp[i][0] = 0
+
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if i < j:
+                break
+            dp[i][j] = max(dp[i-1][j-1] + A[i-1]*j, dp[i-1][j])
+
+    print(dp[n][m])
 
 if __name__ == '__main__':
     main()
